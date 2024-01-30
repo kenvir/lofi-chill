369371e4945a5739375ed0187063a32d550c569e
kenvir <viet02092001@gmail.com>
2024-01-30 21:09:23 +0700

    fix ClickOutSide #1

--- src/components/Control.js
+++ src/components/Control.js
@@ -19,6 +19,7 @@ import light from "@/src/assets/imgs/17.png";
 import start from "@/src/assets/icons/start.svg";
 import timer from "@/src/assets/icons/timer.svg";
 import note from "@/src/assets/icons/note.svg";
+import ClickOutsideWrapper from "./ClickOutSide";
 
 function Control() {
   // Control Menu
@@ -38,16 +39,23 @@ function Control() {
     more == "hidden" ? setMore("block") : setMore("hidden");
   };
 
-  // Total Volume
-  const [volume, setVolume] = useState(0.5);
-  const handleVolume = (event) => {
-    setVolume(parseFloat(event.target.value));
+  const handleOutsideClick = () => {
+    // Update state to hide the element
+    setMode("hidden");
+    setTemplates("hidden");
+    setMore("hidden");
   };
 
+  // Total Volume
+  // const [volume, setVolume] = useState(0.5);
+  // const handleVolume = (event) => {
+  //   setVolume(parseFloat(event.target.value));
+  // };
+
   // Rain Volume
-  const [volumeRain, setVolumeRain] = useState(0.5);
-  const handleVolumeRain = (e) => {
-    setVolumeRain(parseFloat(e.target.value));
+  const [volume, setVolume] = useState(0.5);
+  const handleVolume = (e) => {
+    setVolume(parseFloat(e.target.value));
   };
 
   // Traffic Volume
@@ -57,7 +65,7 @@ function Control() {
   };
 
   // Sound
-  const [rainClick, { stopRain }] = useSound("/sound/rain.mp3", { volumeRain });
+  const [rainClick, { stopRain }] = useSound("/sound/rain.mp3", { volume });
   const [trafficClick, { stopTraffic }] = useSound("/sound/car.mp3", {
     volumeTraffic,
   });
@@ -88,92 +96,94 @@ function Control() {
           alt="mode"
           className="absolute top-[31.2%] right-[27%]"
         />
-        <div className={mode}>
-          <div className="bg-[black] w-[240px] h-[120px] rounded-tr-[50px] absolute right-[0] z-[-1]"></div>
-          <div className="bg-[black] text-white w-[380px] h-[390px] p-6 rounded-xl absolute right-[150px] cursor-default">
-            <div className="flex-col flex">
-              <span className="font-bold text-[22px] mb-4">MOOD</span>
-              <div className="flex justify-between">
-                <div className="flex flex-col items-center w-[90px] bg-white text-black py-3 px-4 rounded-[10px] cursor-pointer">
-                  <Image src={sleep} alt="sleep" className="" />
-                  <span className="font-bold text-[18px] ">Sleepy</span>
-                </div>
-                <div className="flex flex-col items-center w-[90px] bg-white text-black py-3 px-4 rounded-[10px] cursor-pointer">
-                  <Image src={jazz} alt="jazz" className="" />
-                  <span className="font-bold text-[18px] ">Jazzy</span>
-                </div>
-                <div className="flex flex-col items-center w-[90px] bg-white text-black py-3 px-4 rounded-[10px] cursor-pointer">
-                  <Image src={chill} alt="Chill" className="" />
-                  <span className="font-bold text-[18px] ">Chill</span>
+        <ClickOutsideWrapper onClickOutside={handleOutsideClick}>
+          <div className={mode}>
+            <div className="bg-[black] w-[240px] h-[120px] rounded-tr-[50px] absolute right-[0] z-[-1]"></div>
+            <div className="bg-[black] text-white w-[380px] h-[390px] p-6 rounded-xl absolute right-[150px] cursor-default">
+              <div className="flex-col flex">
+                <span className="font-bold text-[22px] mb-4">MOOD</span>
+                <div className="flex justify-between">
+                  <div className="flex flex-col items-center w-[90px] bg-white text-black py-3 px-4 rounded-[10px] cursor-pointer">
+                    <Image src={sleep} alt="sleep" className="" />
+                    <span className="font-bold text-[18px] ">Sleepy</span>
+                  </div>
+                  <div className="flex flex-col items-center w-[90px] bg-white text-black py-3 px-4 rounded-[10px] cursor-pointer">
+                    <Image src={jazz} alt="jazz" className="" />
+                    <span className="font-bold text-[18px] ">Jazzy</span>
+                  </div>
+                  <div className="flex flex-col items-center w-[90px] bg-white text-black py-3 px-4 rounded-[10px] cursor-pointer">
+                    <Image src={chill} alt="Chill" className="" />
+                    <span className="font-bold text-[18px] ">Chill</span>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mt-[40px] flex items-center justify-center gap-3 ">
-              <Image src={silent} alt="silent" className="cursor-pointer" />
-              <input
-                type="range"
-                name=""
-                id="volume"
-                max="1"
-                min="0"
-                step="0.01"
-                value={volume}
-                onChange={handleVolume}
-                className="w-[75%]"
-              />
-              <Image src={loud} alt="loud" className="" />
-            </div>
-            <div className="text-white mt-8">
-              <span className="font-bold text-[22px] ">BACKGROUND NOISE</span>
-              <div className="flex items-center gap-4 mt-4">
-                <span className="font-semibold text-[18px] pr-[17px]">
-                  Rain
-                </span>
-                <Image
-                  src={rain}
-                  alt="rain"
-                  className="cursor-pointer"
-                  onClick={() => {
-                    handleClickRain();
-                  }}
-                />
+              <div className="mt-[40px] flex items-center justify-center gap-3 ">
+                <Image src={silent} alt="silent" className="cursor-pointer" />
                 <input
                   type="range"
                   name=""
-                  id="volumeRain"
-                  max="1"
-                  min="0"
-                  step="0.01"
-                  value={volumeRain}
-                  onChange={handleVolumeRain}
-                  className="w-[56%]"
+                  id="volume"
+                  // max="1"
+                  // min="0"
+                  // step="0.01"
+                  // value={volume}
+                  // onChange={handleVolume}
+                  className="w-[75%]"
                 />
+                <Image src={loud} alt="loud" className="" />
               </div>
-              <div className="flex items-center gap-4 mt-4">
-                <span className="font-semibold text-[18px]">Traffic</span>
-                <Image
-                  src={traffic}
-                  alt="traffic"
-                  className=""
-                  onClick={() => {
-                    handleClickTraffic();
-                  }}
-                />
-                <input
-                  type="range"
-                  name=""
-                  id="volumeTraffic"
-                  max="1"
-                  min="0"
-                  step="0.01"
-                  value={volumeTraffic}
-                  onChange={handleVolumeTraffic}
-                  className="w-[56%]"
-                />
+              <div className="text-white mt-8">
+                <span className="font-bold text-[22px] ">BACKGROUND NOISE</span>
+                <div className="flex items-center gap-4 mt-4">
+                  <span className="font-semibold text-[18px] pr-[17px]">
+                    Rain
+                  </span>
+                  <Image
+                    src={rain}
+                    alt="rain"
+                    className="cursor-pointer"
+                    onClick={() => {
+                      handleClickRain();
+                    }}
+                  />
+                  <input
+                    type="range"
+                    name=""
+                    id="volume"
+                    max="1"
+                    min="0"
+                    step="0.01"
+                    value={volume}
+                    onChange={handleVolume}
+                    className="w-[56%]"
+                  />
+                </div>
+                <div className="flex items-center gap-4 mt-4">
+                  <span className="font-semibold text-[18px]">Traffic</span>
+                  <Image
+                    src={traffic}
+                    alt="traffic"
+                    className="cursor-pointer"
+                    onClick={() => {
+                      handleClickTraffic();
+                    }}
+                  />
+                  <input
+                    type="range"
+                    name=""
+                    id="volumeTraffic"
+                    max="1"
+                    min="0"
+                    step="0.01"
+                    value={volumeTraffic}
+                    onChange={handleVolumeTraffic}
+                    className="w-[56%]"
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </ClickOutsideWrapper>
       </div>
       <div
         className="h-[120px] border-b-[1px] border-[black] relative cursor-pointer"
@@ -184,29 +194,31 @@ function Control() {
           alt="templates"
           className="absolute top-[31.2%] right-[27%]"
         />
-        <div className={templates}>
-          <div className="bg-[black] w-[240px] h-[120px] absolute right-[0] z-[-1]"></div>
+        <ClickOutsideWrapper onClickOutside={handleOutsideClick}>
+          <div className={templates}>
+            <div className="bg-[black] w-[240px] h-[120px] absolute right-[0] z-[-1]"></div>
 
-          <div className="bg-[black] text-white w-[380px] h-[390px] p-6 rounded-xl absolute top-[-130px] right-[150px] cursor-default">
-            <div className="flex-col flex">
-              <span className="font-bold text-[22px] mb-4">TEMPLATES</span>
-              <div className="flex flex-col items-center justify-between gap-5">
-                <div className="flex items-center w-[245px] cursor-pointer">
-                  <Image src={dark} alt="dark" className="rounded-[10px]" />
-                </div>
-                <div className="flex items-center w-[245px] cursor-pointer">
-                  <Image
-                    src={light}
-                    alt="light"
-                    className="rounded-[10px]"
-                    // bgImage={Image.src}
-                    onClick={() => console.log(Image.src)}
-                  />
+            <div className="bg-[black] text-white w-[380px] h-[390px] p-6 rounded-xl absolute top-[-130px] right-[150px] cursor-default">
+              <div className="flex-col flex">
+                <span className="font-bold text-[22px] mb-4">TEMPLATES</span>
+                <div className="flex flex-col items-center justify-between gap-5">
+                  <div className="flex items-center w-[245px] cursor-pointer">
+                    <Image src={dark} alt="dark" className="rounded-[10px]" />
+                  </div>
+                  <div className="flex items-center w-[245px] cursor-pointer">
+                    <Image
+                      src={light}
+                      alt="light"
+                      className="rounded-[10px]"
+                      // bgImage={Image.src}
+                      onClick={() => console.log(Image.src)}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </ClickOutsideWrapper>
       </div>
       <div
         className="h-[120px] relative cursor-pointer"
@@ -217,31 +229,35 @@ function Control() {
           alt="productivity"
           className="absolute top-[31.2%] right-[27%]"
         />
-        <div className={more}>
-          <div className="bg-[black] w-[240px] h-[120px] rounded-br-[50px] absolute right-[0] z-[-1]"></div>
+        <ClickOutsideWrapper onClickOutside={handleOutsideClick}>
+          <div className={more}>
+            <div className="bg-[black] w-[240px] h-[120px] rounded-br-[50px] absolute right-[0] z-[-1]"></div>
 
-          <div className="bg-[black] text-white w-[380px] h-[350px] p-6 rounded-xl absolute top-[-230px] right-[150px] cursor-default">
-            <div className="flex-col flex">
-              <span className="font-bold text-[22px] mb-4">PRODUCTIVITY</span>
-              <div className="flex flex-col justify-between gap-10">
-                <div className="flex items-center bg-[#848484] text-black py-3 px-4 rounded-[10px] gap-3 cursor-pointer">
-                  <Image src={start} alt="start" className="" />
-                  <span className="font-bold text-[18px] ">Start Session</span>
-                </div>
-                <div className="flex items-center bg-[#848484] text-black py-3 px-4 rounded-[10px] gap-3 cursor-pointer">
-                  <Image src={timer} alt="timer" className="" />
-                  <span className="font-bold text-[18px] ">
-                    Timer and Tasks
-                  </span>
-                </div>
-                <div className="flex items-center bg-[#848484] text-black py-3 px-4 rounded-[10px] gap-3 cursor-pointer">
-                  <Image src={note} alt="note" className="" />
-                  <span className="font-bold text-[18px] ">Notes</span>
+            <div className="bg-[black] text-white w-[380px] h-[350px] p-6 rounded-xl absolute top-[-230px] right-[150px] cursor-default">
+              <div className="flex-col flex">
+                <span className="font-bold text-[22px] mb-4">PRODUCTIVITY</span>
+                <div className="flex flex-col justify-between gap-10">
+                  <div className="flex items-center bg-[#848484] text-black py-3 px-4 rounded-[10px] gap-3 cursor-pointer">
+                    <Image src={start} alt="start" className="" />
+                    <span className="font-bold text-[18px] ">
+                      Start Session
+                    </span>
+                  </div>
+                  <div className="flex items-center bg-[#848484] text-black py-3 px-4 rounded-[10px] gap-3 cursor-pointer">
+                    <Image src={timer} alt="timer" className="" />
+                    <span className="font-bold text-[18px] ">
+                      Timer and Tasks
+                    </span>
+                  </div>
+                  <div className="flex items-center bg-[#848484] text-black py-3 px-4 rounded-[10px] gap-3 cursor-pointer">
+                    <Image src={note} alt="note" className="" />
+                    <span className="font-bold text-[18px] ">Notes</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </ClickOutsideWrapper>
       </div>
     </div>
   );
